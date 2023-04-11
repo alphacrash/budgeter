7959ac150e59ed7f06f38f3eca14290126c305ac
alphacrash <sudhansu.ace@gmail.com>
2023-04-11 08:46:48 +0530

    Checkpoint

--- app/index.js
+++ app/index.js
@@ -1,10 +1,16 @@
-import { View, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { View, Text, SafeAreaView } from "react-native";
+import { COLORS } from "@app/constants";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{ header: { backgroundColor: COLORS.lightWhite } }}
+      />
+    </SafeAreaView>
   );
 };
 
